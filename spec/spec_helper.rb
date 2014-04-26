e7f6b9b04a41560351ba8e9cc23909b64e8d4ccc
Jan Graichen <jg@altimos.de>
2014-04-26 09:51:39 +0200

    Adjust spec_helper.rb to not load code before coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,20 @@
 require 'bundler'
-Bundler.require :default, :test
+Bundler.setup :default, :test
 
 if ENV['CI'] || (defined?(:RUBY_ENGINE) && RUBY_ENGINE != 'rbx')
   require 'coveralls'
   Coveralls.wear! do
     add_filter 'spec'
   end
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    Coveralls::SimpleCov::Formatter,
+    SimpleCov::Formatter::HTMLFormatter
+  ]
 end
 
 require 'rbzmq'
 
-Dir[File.expand_path('spec/support/**/*.rb')].each {|f| require f}
+Dir[File.expand_path('spec/support/**/*.rb')].each{|f| require f }
 
 RSpec.configure do |config|
   config.order = 'random'
